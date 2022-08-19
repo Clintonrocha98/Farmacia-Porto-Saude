c8dacce6e2c4f67f7c8a22885c31210ebe91e867
Clinton Rocha <clintongoncalvesrocha@gmail.com>
2022-08-18 22:26:19 -0300

    criação da navegação entre menu e cadastro

---
+++ js/cadastro-cliente-remedio.js
@@ -0,0 +1,74 @@
+import {voltar_menu} from './nav-pag.js';
+const btn_add_cliente = document.querySelector('.add-cliente')
+const btn_add_remedio = document.querySelector('.add-remedio')
+
+export const cadastro_cliente = document.querySelector('.cad-cliente')
+export const cadastro_medicamento = document.querySelector('.cad-remedio')
+
+const add_cliente = document.querySelector('.add-cliente')
+const add_remedio = document.querySelector('.add-remedio')
+var flag = false
+
+let cliente = []
+let remedio = []
+
+let cont = 0
+let cont2 = 0
+
+btn_add_cliente.addEventListener('click',()=>{
+    let nome = document.querySelector('.nome-cliente').value
+    let cpf = document.querySelector('.cpf-cliente').value
+
+    if(nome.length & cpf.length){
+        cliente[cont] = [nome,cpf]
+        cont++
+        flag=true
+    }
+
+})
+
+btn_add_remedio.addEventListener('click',()=>{
+    
+    let codigo = document.querySelector('.cod-remedio').value
+    let nome_remedio = document.querySelector('.nome-remedio').value
+    let qtd = document.querySelector('.quantidade-remedio').value
+    let preco = document.querySelector('.preco-remedio').value
+    let descricao = document.querySelector('.descricao-remedio').value
+    
+    if(
+        codigo.length &
+        nome_remedio.length &
+        qtd.length &
+        preco.length &
+        descricao.length
+        )
+        {
+        remedio[cont2] = [
+            codigo,
+            nome_remedio,
+            qtd,
+            preco,
+            descricao
+        ]
+        cont2++
+        flag = true
+        
+    }
+    
+})
+
+add_cliente.addEventListener('click',()=>{
+    if(flag){
+        voltar_menu(cadastro_cliente)
+    }
+    
+})
+add_remedio.addEventListener('click',()=>{
+    if(flag){
+        voltar_menu(cadastro_medicamento)
+    }
+})
+
+
+    
+
--- js/nav-pag.js
+++ js/nav-pag.js
@@ -1,38 +1,53 @@
-let btn_1 = document.querySelector('.btn-1')
-let btn_2 = document.querySelector('.btn-2')
-let btn_3 = document.querySelector('.btn-3')
-let btn_4 = document.querySelector('.btn-4')
-let btn_5 = document.querySelector('.btn-5')
+import {cadastro_cliente,cadastro_medicamento} from './cadastro-cliente-remedio.js';
 
-let btn_voltar1 = document.querySelector('.btn_voltar1')
-let btn_voltar2 = document.querySelector('.btn_voltar2')
+const btn_1 = document.querySelector('.btn-1')
+// const btn_2 = document.querySelector('.btn-2')
+// const btn_3 = document.querySelector('.btn-3')
+// const btn_4 = document.querySelector('.btn-4')
+// const btn_5 = document.querySelector('.btn-5')
 
-let menu = document.querySelector('.menu')
+const btn_voltar1 = document.querySelector('.btn-voltar1')
+const btn_voltar2 = document.querySelector('.btn-voltar2')
 
-let op_cad_menu = document.querySelector('.choose-registration')
-let op_cad_cliente = document.querySelector('.Cliente')
-let op_cad_medicamento = document.querySelector('.Medicamento')
+const menu = document.querySelector('.menu')
 
-let cadastro_cliente = document.querySelector('.cad-cliente')
-let cadastro_medicamento = document.querySelector('.cad-remedio')
+const op_cad_menu = document.querySelector('.choose-registration')
+const op_cad_cliente = document.querySelector('.Cliente')
+const op_cad_medicamento = document.querySelector('.Medicamento')
+
+function add_remove_menu(){
+    if(menu.classList[1] == "on"){
+        menu.classList.remove('on')
+        menu.classList.add('off')
+    }
+    else{
+        menu.classList.add('on')
+        menu.classList.remove('off')
+    }
+    
+}
+function op_cadastro(){
+    if(op_cad_menu.classList[1] == "off"){
+        op_cad_menu.classList.add('on')
+        op_cad_menu.classList.remove('off')
+    }
+    else{
+        op_cad_menu.classList.add('off')
+        op_cad_menu.classList.remove('on')
+    }
+}
 
 btn_1.addEventListener('click',()=>{
-
-    menu.classList.remove('on')
-    menu.classList.add('off')
-
-    op_cad_menu.classList.remove('off')
-    op_cad_menu.classList.add('on')
-
+    add_remove_menu()
+    
+    op_cadastro()
 })
 op_cad_cliente.addEventListener('click',()=>{
 
     cadastro_cliente.classList.remove('off')
     cadastro_cliente.classList.add('on')
 
-    op_cad_menu.classList.remove('on')
-    op_cad_menu.classList.add('off')
-
+    op_cadastro()
 })
 
 op_cad_medicamento.addEventListener('click',()=>{
@@ -40,25 +55,26 @@ op_cad_medicamento.addEventListener('click',()=>{
     cadastro_medicamento.classList.remove('off')
     cadastro_medicamento.classList.add('on')
 
-    op_cad_menu.classList.remove('on')
-    op_cad_menu.classList.add('off')
+    op_cadastro()
     
 })
 
-btn_voltar1.addEventListener('click',()=>{
-
-    cadastro_cliente.classList.remove('on')
-    cadastro_cliente.classList.add('off')
+export function voltar_menu(a){
+    a.classList.remove('on')
+    a.classList.add('off')
     
-    menu.classList.remove('off')
-    menu.classList.add('on')
+    add_remove_menu()
+}
+
+btn_voltar1.addEventListener('click',()=>{
+    voltar_menu(cadastro_cliente)
 })
 
 btn_voltar2.addEventListener('click',()=>{
-
-    cadastro_medicamento.classList.remove('on')
-    cadastro_medicamento.classList.add('off')
-
-    menu.classList.remove('off')
-    menu.classList.add('on')
+    voltar_menu(cadastro_medicamento)
 })
+
+
+
+
+
